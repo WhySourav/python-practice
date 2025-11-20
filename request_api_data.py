a75f3230a9619e4b16025c7d05bff66a82006dca
sourav <letsmail.souravsharma@gmail.com>
2025-11-20 19:24:16 +0000

    learning about api requests

---
+++ request_api_data.py
@@ -0,0 +1,31 @@
+#how to connect to a api using python
+
+
+#trying too learn about api and stuff 
+
+
+import requests
+
+baseurl = "https://pokeapi.co/api/v2/"
+
+input_name = input("Name of the pokimon: ")
+
+def get_pokemon_data(name):
+    url = f"{baseurl}pokemon/{name.lower()}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        pokemon_data=response.json()
+        return pokemon_data
+    else:
+        print("failed to retrieve data")
+
+pokemoninfo= get_pokemon_data(input_name)
+
+
+if pokemoninfo:
+    print(f"Name: {pokemoninfo["name"]}")
+    print(f"ID: {pokemoninfo["id"]}")
+    print(f"height: {pokemoninfo["height"]}")
+    print(f"weight: {pokemoninfo["weight"]}")
+
